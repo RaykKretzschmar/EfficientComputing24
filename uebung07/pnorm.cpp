b33fe22f828ae9a048825d9d4543c38e0afbc675
Rayk Kretzschmar <112646288+RaykKretzschmar@users.noreply.github.com>
2024-05-17 08:49:21 +0200

    implemented parallel pnorm

--- uebung07/pnorm.cpp
+++ uebung07/pnorm.cpp
@@ -5,6 +5,7 @@
 #include <cstdlib>
 #include <fstream>
 #include <functional>
+#include <cmath>
 
 using namespace std;
 
@@ -39,6 +40,68 @@ void measure_and_print_duration(ofstream &outfile, function<void()> func, const 
     }
 }
 
+double *pnorm(double *x, int n, double p)
+{
+    double *result = new double;
+    *result = 0;
+    // when number of threads = 1
+    if (omp_get_max_threads() == 1)
+    {
+#pragma omp parallel for
+        for (int i = 0; i < n; i++)
+        {
+            *result += abs(x[i]);
+        }
+    }
+    else if (omp_get_max_threads() == 2)
+    {
+#pragma omp parallel for
+        for (int i = 0; i < omp_get_max_threads(); i++)
+        {
+            *result += x[i] * x[i];
+        }
+        *result = sqrt(*result);
+    }
+    else
+    {
+        // max value of x
+        double max = 0;
+
+// find max value of x
+#pragma omp parallel for
+        for (int i = 0; i < n; i++)
+        {
+            if (x[i] > max)
+            {
+                max = abs(x[i]);
+            }
+        }
+    }
+    return result;
+}
+
 int main(int argc, char *argv[])
 {
+    if (argc != 3)
+    {
+        cout << "Usage: " << argv[0] << " <n> <p>" << endl;
+        return 1;
+    }
+
+    int n = 1000000;
+    double p = atof(argv[1]);
+
+    // Initialize vector x with random values
+    double *x = new double[n];
+    initialize_vector(x, n);
+
+    // Measure and print the duration of the pnorm function
+    ofstream outfile("pnorm.csv");
+    measure_and_print_duration(outfile, [&]()
+                               { pnorm(x, n, p); }, "pnorm", n);
+    outfile.close();
+
+    delete[] x;
+
+    return 0;
 }
